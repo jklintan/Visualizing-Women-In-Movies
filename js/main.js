b334159fe47d86b5c98a3aa2a1ead76cd4537070
Josefine Klintberg <josefine.klintberg@hotmail.com>
2020-02-05 11:28:43 +0100

    Test of retreiving movie poster

--- js/main.js
+++ js/main.js
@@ -2,8 +2,11 @@
 
 d3.json("./data/imdb_data.json", function (data) {
 
-    console.log(data[0].Year);
+    console.log(data[2].Poster);
+    var url = data[2].Poster;
 
+    var img = $('<img src=' + 'http://ia.media-imdb.com/images/M/MV5BMTQ5MDY0NDc3MV5BMl5BanBnXkFtZTcwODE4MjgyMQ@@._V1_SY98_CR1,0,67,98_AL_.jpg' + '/>');
+    img.appendTo('body');
 
     //Call for sunburst js
 
